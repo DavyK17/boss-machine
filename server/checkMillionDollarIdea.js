ae5de77ec13d16294cbf6378a616e65ce66161d1
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-22 18:27:22 +0300

    Writing million dollar middleware

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,11 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (req, res, next) => {
+    const { weeklyRevenue, numWeeks } = req.body;
+    const totalRevenue = Number(weeklyRevenue) * Number(numWeeks);
+    if (!weeklyRevenue || !numWeeks || isNaN(weeklyRevenue) || isNaN(numWeeks) || totalRevenue < 1000000) {
+        return res.status(400).send("This is not a million dollar idea!");
+    } 
+    return next();
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
