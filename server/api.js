48cc9c6334aace3c3c612db793b6dce7622552b2
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-12-22 17:03:06 +0300

    DRYing minionId/ideaId handler Sending new/updated objects in POST and PUT request responses

--- server/api.js
+++ server/api.js
@@ -3,41 +3,46 @@ const apiRouter = express.Router();
 
 const db = require("./db");
 
-// MINIONS
-apiRouter.route("/minions")
-    .get((req, res, next) => {
-        return res.send(db.getAllFromDatabase("minions"));
-    })
-    .post((req, res, next) => {
-        db.addToDatabase("minions", req.query);
-        return res.status(201).send(db.getAllFromDatabase("minions"))
-    })
+// Param handler for :minionId and :ideaId
+const paramHandler = (req, res, next, id) => {
+    let model = req.route.path.match(/(?<=\/).+(?=s)/)[0];
 
-apiRouter.param("minionId", (req, res, next, id) => {
     if (req.method === "PUT" && Object.keys(req.query).length === 0) return res.status(400).send();
     try {
-        const minion = db.getFromDatabaseById("minions", id);
-        if (!minion) res.status(404).send("Minion not found!")
+        const obj = db.getFromDatabaseById(`${model}s`, id);
+        if (!obj) res.status(404).send(`Requested ${model} not found!`)
 
-        req.minion = minion;
-        if (req.method === "PUT" && Object.keys(req.query).length !== 0) req.query.id = req.minion.id;
+        req[`${model}`] = obj;
+        if (req.method === "PUT" && Object.keys(req.query).length !== 0) req.query.id = req[`${model}`].id;
         return next();
     } catch(err) {
         return next(err);
     }
-});
+}
+
+// MINIONS
+apiRouter.route("/minions")
+.get((req, res, next) => {
+    return res.send(db.getAllFromDatabase("minions"));
+})
+.post((req, res, next) => {
+    const obj = db.addToDatabase("minions", req.query);
+    return res.status(201).send(obj)
+})
+
+apiRouter.param("minionId", paramHandler);
 apiRouter.route("/minions/:minionId")
-    .get((req, res, next) => {
-        return res.send(req.minion);
-    })
-    .put((req, res, next) => {
-        db.updateInstanceInDatabase("minions", req.query)
-        return res.send("Minion updated successfully!");
-    })
-    .delete((req, res, next) => {
-        db.deleteFromDatabasebyId("minions", req.minion.id)
-        return res.status(204).send();
-    })
+.get((req, res, next) => {
+    return res.send(req.minion);
+})
+.put((req, res, next) => {
+    const obj = db.updateInstanceInDatabase("minions", req.query)
+    return res.send(obj);
+})
+.delete((req, res, next) => {
+    db.deleteFromDatabasebyId("minions", req.minion.id)
+    return res.status(204).send();
+})
 
 // IDEAS
 apiRouter.route("/ideas")
@@ -45,30 +50,18 @@ apiRouter.route("/ideas")
     return res.send(db.getAllFromDatabase("ideas"));
 })
 .post((req, res, next) => {
-    db.addToDatabase("ideas", req.query);
-    return res.status(201).send(db.getAllFromDatabase("ideas"))
+    const obj = db.addToDatabase("ideas", req.query);
+    return res.status(201).send(obj)
 })
 
-apiRouter.param("ideaId", (req, res, next, id) => {
-if (req.method === "PUT" && Object.keys(req.query).length === 0) return res.status(400).send();
-try {
-    const idea = db.getFromDatabaseById("ideas", id);
-    if (!idea) res.status(404).send("Idea not found!")
-
-    req.idea = idea;
-    if (req.method === "PUT" && Object.keys(req.query).length !== 0) req.query.id = req.idea.id;
-    return next();
-} catch(err) {
-    return next(err);
-}
-});
+apiRouter.param("ideaId", paramHandler);
 apiRouter.route("/ideas/:ideaId")
 .get((req, res, next) => {
     return res.send(req.idea);
 })
 .put((req, res, next) => {
-    db.updateInstanceInDatabase("ideas", req.query)
-    return res.send("Idea updated successfully!");
+    const obj = db.updateInstanceInDatabase("ideas", req.query)
+    return res.send(obj);
 })
 .delete((req, res, next) => {
     db.deleteFromDatabasebyId("ideas", req.idea.id)
